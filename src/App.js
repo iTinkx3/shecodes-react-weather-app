3aa2675000757fce42c461a6fbf74ee7901cb49c
iTinkx3 <samantha.collins@live.com.au>
2024-07-19 19:40:24 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by <a href="https://magenta-khapse-6f3ded.netlify.app/" target="_blank" rel="noopener noreferrer">Sam Collins</a> and is open-sourced on{" "} 
       <a href="https://github.com/iTinkx3/shecodes-weather-app" target="_blank" rel="noopener noreferrer">GitHub</a>
       </footer>
+      </div>
       </div>
   );
 }
