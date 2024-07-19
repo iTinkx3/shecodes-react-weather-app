3dedbff77158a296b42c2ecd8f42fdee8de48082
iTinkx3 <samantha.collins@live.com.au>
2024-07-19 19:26:38 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://magenta-khapse-6f3ded.netlify.app/" target="_blank" rel="noopener noreferrer">Sam Collins</a> and is open-sourced on{" "} 
+      <a href="https://github.com/iTinkx3/shecodes-weather-app" target="_blank" rel="noopener noreferrer">GitHub</a>
+      </footer>
+      </div>
   );
 }
